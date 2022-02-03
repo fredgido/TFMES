e8b4c3cf0c8b44b2369e15194225f102aeed9bcf
fredgido <fredgido@gmail.com>
2022-02-03 23:08:05 +0000

    clean code and fixes feat tobbez

--- deepbooru.py
+++ deepbooru.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import hashlib
 import os
-from typing import Union
+from pathlib import Path
+from typing import Union, cast
 
 import keras.backend
 import requests
@@ -9,11 +11,14 @@ from flask import Flask, request, jsonify
 from skimage import transform
 from skimage.transform import AffineTransform
 
-(n_threads := os.getenv("TF_INTER_THREADS")) and tf.config.threading.set_inter_op_parallelism_threads(int(n_threads))
-(n_threads := os.getenv("TF_INTRA_THREADS")) and tf.config.threading.set_intra_op_parallelism_threads(int(n_threads))
+if n_threads := os.getenv("TF_INTER_THREADS"):
+    tf.config.threading.set_inter_op_parallelism_threads(int(n_threads))
+if n_threads := os.getenv("TF_INTRA_THREADS"):
+    tf.config.threading.set_intra_op_parallelism_threads(int(n_threads))
 
-model = tf.keras.models.load_model(os.path.join("model", "model-resnet_custom_v4.h5"))
-imagenet_labels = open(os.path.join("model", "tags.txt")).read().splitlines()
+model = tf.keras.models.load_model(list(Path("model").glob("*.h5"))[0])
+with (Path("model") / "tags.txt").open() as model_file:
+    model_labels = model_file.read().splitlines()
 
 
 def image_to_tensor(image_raw: bytes, width: int, height: int):
@@ -31,22 +36,29 @@ def image_to_tensor(image_raw: bytes, width: int, height: int):
 
 def download_image(url: str, filename: str) -> str:
     url_hash = hashlib.sha256(str(url).encode()).hexdigest()
-    not os.path.exists("images") and not os.path.isdir("images") and os.mkdir("images")
-    if os.path.isfile(os.path.join("images", url_hash, filename)):
-        return os.path.join("images", url_hash, filename)
-    os.mkdir(os.path.join("images", url_hash))
+    file_path = Path("images") / url_hash / filename
+
+    if file_path.is_file():
+        return str(file_path)
+
+    file_path.parent.mkdir(exist_ok=True, parents=True)
+
     r = requests.get(url, timeout=5, headers={"referer": "https://www.pixiv.net/"} if "pximg" in url else {})
     if r.status_code != 200:
-        raise Exception("error " + str(r.status_code) + " on " + url + "  " + r.text)
-    with open(os.path.join("images", url_hash, filename), "wb") as f:
+        raise Exception(f"Error {r.status_code} on url {url}: {r.text}")
+
+    with file_path.open("wb") as f:
         f.write(r.content)
-    return os.path.join("images", url_hash, filename)
+
+    return str(file_path)
 
 
-def process_images(input_images: list[Union[str, bytes]], min_score: float = 0.1) -> list[list[tuple[str, float]]]:
+def process_images(
+    input_images: Union[list[str], list[bytes]], min_score: float = 0.1
+) -> list[list[tuple[str, float]]]:
     if isinstance(input_images[0], str):
         input_images = [tf.io.read_file(image_path) for image_path in input_images]
-    images = [image_to_tensor(image_path, model.input_shape[1], model.input_shape[2]) for image_path in input_images]
+    images = [image_to_tensor(cast(bytes, file), model.input_shape[1], model.input_shape[2]) for file in input_images]
     images = [image.reshape((1, image.shape[0], image.shape[1], image.shape[2])) for image in images]
 
     if len(images) > 10:
@@ -58,8 +70,8 @@ def process_images(input_images: list[Union[str, bytes]], min_score: float = 0.1
 
     outputs = []
     for prediction in predictions:
-        tag_score = sorted(zip(list(prediction), list(range(0, len(prediction) - 1))), key=lambda x: x[0], reverse=True)
-        output = [(imagenet_labels[tag], score) for score, tag in tag_score if score > min_score]
+        tag_score = sorted(zip(prediction, range(0, len(prediction))), key=lambda x: x[0], reverse=True)
+        output = [(model_labels[tag], score) for score, tag in tag_score if score > min_score]
         # pprint.pprint(dict(output)['realistic'])
         # pprint.pprint(dict(output)['photorealistic'])
         # pprint.pprint(output[:50])
@@ -85,11 +97,11 @@ def evaluate():
     evaluation = process_images(
         [download_image(url, url.split("/")[-1]) for url in urls.split(",")]
         if urls
-        else [file.stream.seek(0) or file.stream.read() for file in files],
+        else [file.stream.seek(0) or file.stream.read() for file in files],  # noqa:F823
         float(request.args.get("min_score", 0)),
     )
     return jsonify(evaluation if len(evaluation) > 1 else evaluation[0])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("PORT") or 8000, debug=os.getenv("DEBUG") == "True")
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT") or 8000), debug=os.getenv("DEBUG") == "True")
